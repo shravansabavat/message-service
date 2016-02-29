f4a395a547e26962c7e8e0a053285410f46d1a3e
shravansabavat <shravan.sabavat@gmail.com>
2016-02-28 20:45:41 -0500

    initial code

--- app/lib/webApp.js
+++ app/lib/webApp.js
@@ -18,10 +18,20 @@ var corsOptions = {
 var app = express();
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
+
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
 app.use(PALLYNDROME_ENDPOINT, pallyndromeRouter);
 app.use(HEALTHCHECK_ENDPOINT, healthCheckRouter);
 
 
-app.listen(PORT, function (req, res) {
-    console.log('Server started and running on port:' + PORT)
-});
+
+
+app.listen(PORT);
+console.log('Magic happens on port ' + PORT);
+
+exports = module.exports = app; 						// expose app
