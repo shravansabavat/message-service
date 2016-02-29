bfbf49357bead5159e076280ac482a1283c7e5e8
shravansabavat <shravan.sabavat@gmail.com>
2016-02-28 19:37:35 -0500

    initial code

--- app/lib/webApp.js
+++ app/lib/webApp.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pallyndromeRouter = require('./routers/pallyndromeRouter');
+var healthCheckRouter = require('./routers/healthcheckRouter');
 var cors = require('cors')
 
 var PALLYNDROME_ENDPOINT = '/pallyndrome';
+var HEALTHCHECK_ENDPOINT = '/healthcheck';
+
 var PORT = 8081;
 var corsOptions = {
     "origin": "*",
@@ -16,7 +19,7 @@ var app = express();
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(PALLYNDROME_ENDPOINT, pallyndromeRouter);
-
+app.use(HEALTHCHECK_ENDPOINT, healthCheckRouter);
 
 
 app.listen(PORT, function (req, res) {
